d09cee5e788d8e4d9af08a55904eebdb7ba1d8ca
Brenda Sutara <sutarabrendagmail.com>
2025-02-28 10:00:11 -0300

    camera watch current location

---
+++ src/presentation/components/maps/Map.tsx
@@ -0,0 +1,71 @@
+import { Platform } from "react-native"
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps"
+import { Location } from "../../../infrastructure/interfaces/location"
+import { FAB } from "../ui/FAB"
+import { useRef } from "react"
+import { useLocationStore } from "../../store/location/useLocationStore"
+
+interface Props {
+    showsUserLocation?: boolean,
+    initialLocation: Location
+}
+
+export const Map = ({ showsUserLocation = true, initialLocation }: Props) => {
+
+    const mapRef = useRef<MapView>();
+    const cameraLocation = useRef<Location>( initialLocation )
+
+
+    const { getLocation, lastKnownLocation } = useLocationStore()
+
+    const moveCameraToLocation = (location: Location) => {
+        if (!mapRef.current) return;
+
+        mapRef.current.animateCamera({center:location})
+    }
+
+    const moveToCurrentLocation = async() => {
+        if (!lastKnownLocation) {
+            moveCameraToLocation(initialLocation)
+        }
+        const location = await getLocation();
+
+        if (!location) return;
+
+        moveCameraToLocation(location)
+    }
+
+    return (
+        <>
+         <MapView
+                ref={ map => mapRef.current = map! }
+                showsUserLocation={ showsUserLocation}
+                provider={ Platform.OS === 'ios' ? undefined : PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={{ flex: 1 }}
+                region={{
+                    latitude: cameraLocation.current.latitude,
+                    longitude: cameraLocation.current.longitude,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}
+            >
+                {/* <Marker
+                    coordinate={{
+                        latitude: 37.78825,
+                        longitude: -122.4324
+                    }}
+                    title="Ubicación"
+                    description="Esta es la descripción"
+                    image={ require('../../../assets/custom-marker.png')}/> */}
+            </MapView>
+            <FAB 
+                iconName="compass-outline"
+                onPress={moveToCurrentLocation}
+                style={{
+                    bottom: 20,
+                    right:20
+                }}
+            />   
+        </>
+    )
+}
---
+++ src/presentation/screens/maps/MapScreen.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, View } from "react-native"
+import { useLocationStore } from "../../store/location/useLocationStore";
+import { LoadingScreen } from "../loading/LoadingScreen";
+import { useEffect } from "react";
+import { Map } from "../../components/maps/Map";
+
+export const MapScreen = () => {
+
+    const { lastKnownLocation, getLocation } = useLocationStore();
+
+    useEffect(()=>{
+        if (lastKnownLocation === null) {
+          getLocation()  
+        }
+    }, [])
+
+    if (lastKnownLocation === null ) {
+        return(<LoadingScreen />)
+    }
+
+    return (
+        <View style={styles.container}>
+            <Map
+                initialLocation={ lastKnownLocation }
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+    },
+   });
